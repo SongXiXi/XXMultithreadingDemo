a6c8f6dedde8bef864fbbca9529b539defa5bd38
songwei <songwei@zhuanzhuan.com>
2018-07-23 14:28:21 +0800

    init

---
+++ XXMultithreadingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XXMultithreadingDemo
+//
+//  Created by Jayko on 2018/7/23.
+//  Copyright © 2018年 iGIngko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
